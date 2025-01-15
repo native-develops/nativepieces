2e07d918a5ee390ae201331c3b1c2f4b7dadabf2
native-develops <arsenedjumbe0@gmail.com>
2025-01-15 21:16:48 +0300

    Adjustments

--- js/load_gallery.js
+++ js/load_gallery.js
@@ -1,23 +1,20 @@
 const imgContainer = document.getElementById("imgContainer")
 const row = document.createElement('div')
-row.setAttribute("class", "")
+row.setAttribute("class", "row gy-3")
 
 for (let i = 1; i <= 69; i++) {
 
-    row.innerHTML += `
-  
-       
-							<div class="col-md-6 col-lg-4 d-flex">
-								
-									<img src="./images/gallery/work_image (${i}).jpg" alt="Image" class="img-fluid">
-									
-								
-							</div>
-				
-             
+    const col = document.createElement('div')
+    col.setAttribute("class", "col-md-6 col-lg-4 d-flex align-items-start")
+    col.innerHTML += `
+
+        <img src="./images/gallery/work_image (${i}).jpg" alt="Image" class="img-fluid img_contain">
 
     `
- 
+    
+   row.append(col)
     imgContainer.append(row)
 }
+
+
 
